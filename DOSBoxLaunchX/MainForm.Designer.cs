900b3edcd4dca7602ceb20cb420f8cc99eca2c3e
Jez <jez9999@gmail.com>
2025-10-23 20:54:36 +0100

    Added icon.

--- DOSBoxLaunchX/MainForm.Designer.cs
+++ DOSBoxLaunchX/MainForm.Designer.cs
@@ -20,20 +20,49 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "DOSBoxLaunchX";
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			txtOutput = new TextBox();
+			SuspendLayout();
+			// 
+			// txtOutput
+			// 
+			txtOutput.BackColor = SystemColors.Window;
+			txtOutput.Font = new Font("Lucida Console", 9.75F);
+			txtOutput.Location = new Point(12, 692);
+			txtOutput.Multiline = true;
+			txtOutput.Name = "txtOutput";
+			txtOutput.ReadOnly = true;
+			txtOutput.ScrollBars = ScrollBars.Vertical;
+			txtOutput.Size = new Size(1235, 163);
+			txtOutput.TabIndex = 0;
+			txtOutput.TabStop = false;
+			// 
+			// MainForm
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(1259, 867);
+			Controls.Add(txtOutput);
+			FormBorderStyle = FormBorderStyle.FixedSingle;
+			Icon = (Icon)resources.GetObject("$this.Icon");
+			MaximizeBox = false;
+			MinimizeBox = false;
+			Name = "MainForm";
+			Text = "DOSBoxLaunchX";
+			Load += MainForm_Load;
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
-    }
+		#endregion
+
+		private TextBox txtOutput;
+	}
 }
--- DOSBoxLaunchX/MainForm.cs
+++ DOSBoxLaunchX/MainForm.cs
@@ -1,10 +1,11 @@
-namespace DOSBoxLaunchX
-{
-    public partial class MainForm : Form
-    {
-        public MainForm()
-        {
-            InitializeComponent();
-        }
-    }
+namespace DOSBoxLaunchX {
+	public partial class MainForm : Form {
+		public MainForm() {
+			InitializeComponent();
+		}
+
+		private void MainForm_Load(object sender, EventArgs ea) {
+			txtOutput.Text = "Hello world!";
+		}
+	}
 }
