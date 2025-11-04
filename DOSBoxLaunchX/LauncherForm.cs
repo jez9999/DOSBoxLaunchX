b7e51a27fd8fdb4177c6aeb04404b7af4d136bf4
Jez <jez9999@gmail.com>
2025-11-04 01:06:56 +0000

    Improve launch messaging.

--- DOSBoxLaunchX/LauncherForm.cs
+++ DOSBoxLaunchX/LauncherForm.cs
@@ -73,10 +73,11 @@ public partial class LauncherForm : Form {
 			return false;
 		}
 		var shortcutSettings = _launchSettingsFileService.LoadFromFile(dlxPath);
-		addTxtboxMsg("");
 		if (!string.IsNullOrWhiteSpace(shortcutSettings.Name)) {
-			addTxtboxMsg(shortcutSettings.Name);
-			addTxtboxMsg(new string('=', shortcutSettings.Name.Length));
+			var heading = $"| {shortcutSettings.Name} |";
+			addTxtboxMsg("/" + new string('=', heading.Length - 2) + "\\");
+			addTxtboxMsg(heading);
+			addTxtboxMsg("\\" + new string('=', heading.Length - 2) + "/");
 		}
 
 		addTxtboxMsg("Loading base DOSBox config...");
